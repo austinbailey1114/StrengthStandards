9de79d764a3a394167dacf577b6635bcf56eed11
Austin Bailey <austinbailey@Austins-MacBook-Pro-2.local>
2017-07-20 10:16:13 -0400

    Added Input

--- Lift.java
+++ Lift.java
@@ -6,14 +6,14 @@ public class Lift {
 	double reps;
 	String liftType;
 	double wilks_constants[] = {-216.0475144, 16.2606339, -0.002388645, -0.00113732, 7.01863e-06, -1.291e-08};
-	Lift(double w, double r, double bw, String l) {
-		bodyWeight = bw;
-		weight = w;
-		reps = r;
+	Lift(String w, String r, String bw, String l) {
+		bodyWeight = Double.parseDouble(bw);
+		weight = Double.parseDouble(w);
+		reps = Double.parseDouble(r);
 		liftType = l;
 	}
 	
-	public double max() {
+	private double max() {
 		return weight *(1 +(reps/30));
 	}
 	private double wilks() {
@@ -25,8 +25,13 @@ public class Lift {
 		return this.max() * this.wilks();
 	}
 	
-	public void printEstimatedReps() {
-		
+	private void printEstimatedReps() {
+		System.out.println("\tEstimated Reps at % of One Rep Maximum");
+		System.out.println("Reps:  1     2     3     4     5     6     7     8     9     10");
+		System.out.println("%:    100    95    90    88    86    83    80    78    76    75");
+		System.out.println("Wght: " + (int) this.max() + "   " + (int) (this.max() * .95) + "   " + (int) (this.max() * .9) + "   " + 
+				(int) (this.max() * .88) + "   " + (int) (this.max() * .86) + "   " + (int) (this.max() * .83) + "   " + (int) (this.max() * .8)
+				+ "   " + (int) (this.max() * .78) + "   " + (int) + (this.max() * .76) + "   " + (int) (this.max() * .75));
 	}
 	
 	public void printLiftInfo() {
@@ -36,46 +41,46 @@ public class Lift {
 		System.out.println();
 		System.out.println("Level: " + this.levelChecker());
 		System.out.println();
-		System.out.println("Percentages will go here");
+		this.printEstimatedReps();
 	}
 	
 	public String levelChecker() {
-		double untrainedS = 79.58; double noviceS = 147.51; double intermediateS = 178.42; double advancedS = 244.93; double eliteS = 319.72;
-		double untrainedD = 99.48; double noviceD = 183.18; double intermediateD = 211.31; double advancedD = 286.78; double eliteD = 348.53; 
-		double untrainedI = 54.89; double noviceI = 82.33; double intermediateI = 113.20; double advancedI = 164; double eliteI = 209.26;
+		double noviceS = 147.51; double intermediateS = 178.42; double advancedS = 244.93; double eliteS = 319.72;
+		double noviceD = 183.18; double intermediateD = 211.31; double advancedD = 286.78; double eliteD = 348.53; 
+		double noviceI = 82.33; double intermediateI = 113.20; double advancedI = 164; double eliteI = 209.26;
 
-		if (this.liftType == "deadlift") {
-			if((this.wilksWeight() >= untrainedD) & (this.wilksWeight() < noviceD)) {
+		if (this.liftType.equals("deadlift")) {
+			if((this.wilksWeight() < noviceD)) {
 				return "Untrained";
-			} else if((this.wilksWeight() >= noviceD) & (this.wilksWeight() < intermediateD)) {
+			} else if((this.wilksWeight() >= noviceD) && (this.wilksWeight() < intermediateD)) {
 				return "Novice";
-			} else if((this.wilksWeight() >= intermediateD) & (this.wilksWeight() < advancedD)) {
+			} else if((this.wilksWeight() >= intermediateD) && (this.wilksWeight() < advancedD)) {
 				return "Intermediate";
-			} else if((this.wilksWeight() >= advancedD) & (this.wilksWeight() < eliteD)) {
+			} else if((this.wilksWeight() >= advancedD) && (this.wilksWeight() < eliteD)) {
 				return "Advanced";
 			}
 			else return "Elite";
 		}
-		else if (this.liftType == "squat") {
-			if((this.wilksWeight() >= untrainedS) & (this.wilksWeight() < noviceS)) {
+		else if (this.liftType.equals("squat")) {
+			if((this.wilksWeight() < noviceS)) {
 				return "Untrained";
-			} else if((this.wilksWeight() >= noviceS) & (this.wilksWeight() < intermediateS)) {
+			} else if((this.wilksWeight() >= noviceS) && (this.wilksWeight() < intermediateS)) {
 				return "Novice";
-			} else if((this.wilksWeight() >= intermediateS) & (this.wilksWeight() < advancedS)) {
+			} else if((this.wilksWeight() >= intermediateS) && (this.wilksWeight() < advancedS)) {
 				return "Intermediate";
-			} else if((this.wilksWeight() >= advancedS) & (this.wilksWeight() < eliteS)) {
+			} else if((this.wilksWeight() >= advancedS) && (this.wilksWeight() < eliteS)) {
 				return "Advanced";
 			}
 			else return "Elite";
 		}
-		else if (this.liftType == "bench") {
-			if((this.wilksWeight() >= untrainedI) & (this.wilksWeight() < noviceI)) {
+		else if (this.liftType.equals("bench")) {
+			if((this.wilksWeight() < noviceI)) {
 				return "Untrained";
-			} else if((this.wilksWeight() >= noviceI) & (this.wilksWeight() < intermediateI)) {
+			} else if((this.wilksWeight() >= noviceI) && (this.wilksWeight() < intermediateI)) {
 				return "Novice";
-			} else if((this.wilksWeight() >= intermediateI) & (this.wilksWeight() < advancedI)) {
+			} else if((this.wilksWeight() >= intermediateI) && (this.wilksWeight() < advancedI)) {
 				return "Intermediate";
-			} else if((this.wilksWeight() >= advancedI) & (this.wilksWeight() < eliteI)) {
+			} else if((this.wilksWeight() >= advancedI) && (this.wilksWeight() < eliteI)) {
 				return "Advanced";
 			}
 			else return "Elite";
--- Training.java
+++ Training.java
@@ -1,10 +1,21 @@
 package lift;
+import java.util.*;
 
 public class Training {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Lift test = new Lift(325, 5, 83, "deadlift");
+		System.out.println("Enter your lift in the following format:");
+		System.out.println("lift,bodyweight,liftweight,liftreps");
+		System.out.println();
+		System.out.print("Enter here: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		scanner.close();
+		String info[] = input.split(",");
+		
+		Lift test = new Lift(info[2], info[3], info[1], info[0]);
 		test.printLiftInfo();
 	}
 
